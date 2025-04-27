d967a4f64c330018d2ce49ed824fa91687952446
Emre <98234597+emrecam13@users.noreply.github.com>
2025-04-27 20:08:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ if module == "Overview & Data Summary":
     # 3a) Distributions
     st.subheader("🔹 Distributions of Raw Numeric Features")
     raw_num_cols = ["age","height_cm","weight_kg","overall","potential"]
-    fig, axes = plt.subplots(len(raw_num_cols),5,figsize=(8,4*len(raw_num_cols)))
+    fig, axes = plt.subplots(len(raw_num_cols),1,figsize=(8,4*len(raw_num_cols)))
     for ax,col in zip(axes, raw_num_cols):
         sns.histplot(df_model[col], bins=30, kde=True, ax=ax)
         ax.set_title(f"{col} Distribution")
